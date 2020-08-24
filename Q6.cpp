9b968115f614dfafdfbcc4585f66ee25754e6bd3
Shwetansh Sharma <69001506+Prophet1702@users.noreply.github.com>
2020-08-24 17:37:00 +0530

    Assignment 1 Q6

---
+++ Q6.cpp
@@ -0,0 +1,17 @@
+#include<iostream>
+using namespace std;
+int main()
+{
+    int i=0,n;
+    cin>>n;
+    for(i=2;i<n;i++)
+    {
+        if(n%i==0)
+        {
+            cout<<"Not a prime number";
+            return 0;
+        }
+    }
+    cout<<"It's a prime number";
+    return 0;
+}
