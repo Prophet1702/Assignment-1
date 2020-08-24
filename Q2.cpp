357a2fae1cac135c11844a14376e73a33731cf75
Shwetansh Sharma <69001506+Prophet1702@users.noreply.github.com>
2020-08-24 16:04:30 +0530

    Update Q2.cpp

--- Q2.cpp
+++ Q2.cpp
@@ -1,14 +1,35 @@
-#include<iostream>
-using namespace std;
-int main()
-{
-	int max,a[3];
-	for(int i=0;i<3;i++)
-		cin>>a[i];
-	max=a[0];
-	for(int i=0;i<3;i++)
-		if(a[i]>max)
-			max=a[i];
-	cout<<endl<<max;
-	return 0;
-}
+#include <iostream> 
+using namespace std; 
+  
+void swap(int *xp, int *yp)  
+{  
+    int temp = *xp;  
+    *xp = *yp;  
+    *yp = temp;  
+}  
+  
+void selectionSort(int arr[], int n)  
+{  
+    int i, j, min_idx;  
+    for (i = 0; i < n-1; i++)  
+    {  
+       
+        min_idx = i;  
+        for (j = i+1; j < n; j++)  
+        if (arr[j] < arr[min_idx])  
+            min_idx = j;  
+   
+        swap(&arr[min_idx], &arr[i]);  
+    }  
+}  
+int main()  
+{  
+	int n;
+	cin>>n;
+    int arr[n];    
+	for(int i=0;i<n;i++)
+		cin>>arr[i];
+    selectionSort(arr, n);  
+	cout<<"Second smallest no. = "<<arr[1]<<endl<<"Second largest no, = "<<arr[n-2];
+    return 0;  
+}  
