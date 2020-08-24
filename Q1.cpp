d2f3215a8a5d4c6f796fc40847bc72084fd0f284
Shwetansh Sharma <69001506+Prophet1702@users.noreply.github.com>
2020-08-24 16:14:01 +0530

    Assignment 1 Q1

---
+++ Q1.cpp
@@ -0,0 +1,14 @@
+#include<iostream>
+using namespace std;
+int main()
+{
+        int max,a[3];
+        for(int i=0;i<3;i++)
+                cin>>a[i];
+        max=a[0];
+        for(int i=0;i<3;i++)
+                if(a[i]>max)
+                        max=a[i];
+        cout<<endl<<max;
+        return 0;
+}
